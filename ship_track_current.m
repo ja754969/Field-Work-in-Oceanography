a8585203a33f4fd68fb3c5b02383724b61e9b929
Yu-Hao Tseng <jaden754969@gmail.com>
2022-01-03 20:15:12 +0800

    add function

--- ship_track_current.m
+++ ship_track_current.m
@@ -5,31 +5,32 @@ load('./C1267/ADCP_Nan_Bay.mat')
 LAT_lim = [21+50/60:5/60:22];
 LON_lim = [120+40/60:5/60:120+55/60];
 %%
-layer = 2;
-current_depth = 12 + 8*layer; % meter;
+layer = 3;
+current_depth = 12 + 8*(layer-1); % meter;
 u = SerEmmpersec(:,layer);
 v = SerNmmpersec(:,layer);
 %%
-filter_ind_1 = find(u<-30000 | (SerHour<=23 & SerDay==25) | ...
+filter_ind_head = find(u<-30000 | (SerHour<=23 & SerDay==25) | ...
     (SerHour<=20 & SerDay==26) | ...
     (SerMin<= 6 & SerHour<=21 & SerDay==26));
-filter_ind_2 = find(v<-30000 | (SerMin>=53 & SerHour>=21 & SerDay==27) | ...
+filter_ind_tail = find(v<-30000 | (SerMin>=53 & SerHour>=21 & SerDay==27) | ...
     (SerHour>=22 & SerDay==27));
-u([filter_ind_1;filter_ind_2])=[];
-v([filter_ind_1;filter_ind_2])=[];
-AnLLatDeg([filter_ind_1;filter_ind_2]) = [];
-AnLLonDeg([filter_ind_1;filter_ind_2]) = [];
+u([filter_ind_head;filter_ind_tail])=[];
+v([filter_ind_head;filter_ind_tail])=[];
+AnLLatDeg([filter_ind_head;filter_ind_tail]) = [];
+AnLLonDeg([filter_ind_head;filter_ind_tail]) = [];
 %%
-SerYear([filter_ind_1;filter_ind_2]) = [];
-SerMon([filter_ind_1;filter_ind_2]) = [];
-SerDay([filter_ind_1;filter_ind_2]) = [];
-SerHour([filter_ind_1;filter_ind_2]) = [];
-SerMin([filter_ind_1;filter_ind_2]) = [];
-SerSec([filter_ind_1;filter_ind_2]) = [];
+SerYear([filter_ind_head;filter_ind_tail]) = [];
+SerMon([filter_ind_head;filter_ind_tail]) = [];
+SerDay([filter_ind_head;filter_ind_tail]) = [];
+SerHour([filter_ind_head;filter_ind_tail]) = [];
+SerMin([filter_ind_head;filter_ind_tail]) = [];
+SerSec([filter_ind_head;filter_ind_tail]) = [];
+time_series = datetime(SerYear+2000,SerMon,SerDay,SerHour,SerMin,SerSec);
 %%
 fig = figure;
 fig.PaperUnits = 'centimeters';
-fig.PaperSize = [29.7 21]; % A4 papersize (horizontal,21-by-29.7 cm,[width height])
+fig.PaperSize = [21 29.7]; % A4 papersize (horizontal,21-by-29.7 cm,[width height])
 fig.PaperType = '<custom>';
 fig.WindowState = 'maximized';
 fig
@@ -66,18 +67,13 @@ text(0.8,0.2,['Current depth : ' num2str(current_depth) ' m'],'FontSize',15);
 % ax.FontSize = 15;
 %%
 ax1 = axes;
-ax1.Position = [0.05 0.6 0.3 0.3];
+ax1.Position = [0.01 0.69 0.28 0.28];
 trajectory_ind_1 = find((SerMin>= 6 & SerHour<=21 & SerDay==26) | ...
     (SerMin>= 0 & SerHour==22 & SerDay==26) | ...
     (SerMin>= 0 & SerHour==23 & SerDay==26) | ...
     (SerMin<= 37 & SerHour==0 & SerDay==27));
-ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_1,LON_lim,LAT_lim)
-title(['2007/12/' num2str(SerDay(trajectory_ind_1(1))) ' ' ...
-        num2str(SerHour(trajectory_ind_1(1))) ':' num2str(SerMin(trajectory_ind_1(1))) ...
-        ':' num2str(SerSec(trajectory_ind_1(1))) ' ~ ' ...
-        '2007/12/' num2str(SerDay(trajectory_ind_1(end))) ' ' ...
-        num2str(SerHour(trajectory_ind_1(end))) ':' num2str(SerMin(trajectory_ind_1(end))) ...
-        ':' num2str(SerSec(trajectory_ind_1(end)))],'FontSize',20) ;
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_1,...
+    LON_lim,LAT_lim,time_series)
 ax1.FontSize = 15;
 % c1 = colorbar;
 [X1,cmap1] = imread('sst.png');
@@ -96,35 +92,81 @@ m_text(120+43/60,21+59/60,'#1','Color','w','FontSize',20,'FontWeight','bold');
 % fig
 %%
 ax2 = axes;
-ax2.Position= [0.35 0.6 0.3 0.3];
+ax2.Position= [0.31 0.69 0.28 0.28];
 trajectory_ind_2 = find((SerMin> 37 & SerHour==0 & SerDay==27) | ...
     (SerHour==1 & SerDay==27) | (SerHour==2 & SerDay==27) | ...
     (SerHour==3 & SerDay==27) | (SerMin<=7 & SerHour==4 & SerDay==27));
-ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_2,LON_lim,LAT_lim)
-title(['2007/12/' num2str(SerDay(trajectory_ind_2(1))) ' ' ...
-        num2str(SerHour(trajectory_ind_2(1))) ':' num2str(SerMin(trajectory_ind_2(1))) ...
-        ':' num2str(SerSec(trajectory_ind_2(1))) ' ~ ' ...
-        '2007/12/' num2str(SerDay(trajectory_ind_2(end))) ' ' ...
-        num2str(SerHour(trajectory_ind_2(end)))],'FontSize',20) ;
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_2,...
+    LON_lim,LAT_lim,time_series)
 ax2.FontSize = 15;
-% c1 = colorbar;
-[X1,cmap1] = imread('sst.png');
-RGB1 = ind2rgb(X1,cmap1);
-colormap(ax1,reshape(RGB1(10,:,:),size(RGB1,2),3));
-% ---imread colormap---%
-c1.Label.String = 'depth (m)';
+colormap(ax2,reshape(RGB1(10,:,:),size(RGB1,2),3));
 caxis([-800 0])
-m_text(120+43/60,21+59/60,'#1','Color','w','FontSize',20,'FontWeight','bold');
+m_text(120+43/60,21+59/60,'#2','Color','w','FontSize',20,'FontWeight','bold');
+%%
+ax3 = axes;
+ax3.Position= [0.61 0.69 0.28 0.28];
+trajectory_ind_3 = find((SerMin > 7 & SerHour==4 & SerDay==27) | ...
+    (SerHour==5 & SerDay==27) | (SerHour==6 & SerDay==27) | ...
+    (SerMin<=53 & SerHour==7 & SerDay==27));
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_3,...
+    LON_lim,LAT_lim,time_series)
+ax3.FontSize = 15;
+colormap(ax3,reshape(RGB1(10,:,:),size(RGB1,2),3));
+caxis([-800 0])
+m_text(120+43/60,21+59/60,'#3','Color','w','FontSize',20,'FontWeight','bold');
 
 
+%%
+ax4 = axes;
+ax4.Position= [0.01 0.34 0.28 0.28];
+trajectory_ind_4 = find((SerMin > 53 & SerHour==7 & SerDay==27) | ...
+    (SerHour==8 & SerDay==27) | (SerHour==9 & SerDay==27) | ...
+    (SerHour==10 & SerDay==27) | (SerMin<=12 & SerHour==11 & SerDay==27));
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_4,...
+    LON_lim,LAT_lim,time_series)
+ax4.FontSize = 15;
+colormap(ax4,reshape(RGB1(10,:,:),size(RGB1,2),3));
+caxis([-800 0])
+m_text(120+43/60,21+59/60,'#4','Color','w','FontSize',20,'FontWeight','bold');
 
 
+%%
+ax5 = axes;
+ax5.Position= [0.31 0.34 0.28 0.28];
+trajectory_ind_5 = find((SerMin > 12 & SerHour==11 & SerDay==27) | ...
+    (SerHour==12 & SerDay==27) | (SerHour==13 & SerDay==27) | ...
+    (SerMin<=38 & SerHour==14 & SerDay==27));
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_5,...
+    LON_lim,LAT_lim,time_series)
+ax5.FontSize = 15;
+colormap(ax5,reshape(RGB1(10,:,:),size(RGB1,2),3));
+caxis([-800 0])
+m_text(120+43/60,21+59/60,'#5','Color','w','FontSize',20,'FontWeight','bold');
+%%
+ax6 = axes;
+ax6.Position= [0.61 0.34 0.28 0.28];
+trajectory_ind_6 = find((SerMin > 38 & SerHour==14 & SerDay==27) | ...
+    (SerHour==15 & SerDay==27) | (SerHour==16 & SerDay==27) | ...
+    (SerHour==17 & SerDay==27) | (SerMin<=22 & SerHour==18 & SerDay==27));
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_6,...
+    LON_lim,LAT_lim,time_series)
+ax6.FontSize = 15;
+colormap(ax6,reshape(RGB1(10,:,:),size(RGB1,2),3));
+caxis([-800 0])
+m_text(120+43/60,21+59/60,'#6','Color','w','FontSize',20,'FontWeight','bold');
 
-
-
-
-
-
+%%
+ax7 = axes;
+ax7.Position= [0.01 0.009 0.28 0.28];
+trajectory_ind_7 = find((SerMin > 22 & SerHour==18 & SerDay==27) | ...
+    (SerHour==19 & SerDay==27) | (SerHour==20 & SerDay==27) | ...
+    (SerMin<=54 & SerHour==21 & SerDay==27) | (SerHour==22 & SerDay==27));
+ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind_7,...
+    LON_lim,LAT_lim,time_series)
+ax7.FontSize = 15;
+colormap(ax7,reshape(RGB1(10,:,:),size(RGB1,2),3));
+caxis([-800 0])
+m_text(120+43/60,21+59/60,'#7','Color','w','FontSize',20,'FontWeight','bold');
 
 %%
 mkdir('./results');
--- ship_track_current_function.m
+++ ship_track_current_function.m
@@ -1,4 +1,4 @@
-function ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind,LON_lim,LAT_lim)
+function ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind,LON_lim,LAT_lim,time)
     m_proj('miller','lon',[LON_lim(1) LON_lim(end)],'lat',[LAT_lim(1) LAT_lim(end)]);
     [ELEV,LONG,LAT] = m_etopo2([118 123 20 30]); % [ELEV,LONG,LAT]=m_etopo2([LONG_MIN LONG_MAX LAT_MIN LAT_MAX])
     m_pcolor(LONG,LAT,ELEV);shading interp
@@ -10,13 +10,18 @@ function ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind,LON_
         u(trajectory_ind),v(trajectory_ind),0);
     scale = 0.00001;
     mqr.Color = 'b';
+    mqr.LineWidth = 1;
+    mqr.LineStyle = '-';
+    mqr.MarkerFaceColor = 'b';
+    mqr.MaxHeadSize = 1;
     hU1 = get(mqr,'UData');
     hV1 = get(mqr,'VData');
     set(mqr,'UData',scale*hU1,'VData',scale*hV1)
 
     hold on;
     m_gshhs_f('patch',[0 0 0],'linewidth',0.5);
-    m_grid('tickdir','in','xtick',LON_lim,'ytick',LAT_lim,'fontsize',15)
+    m_grid('tickdir','out','xtick',LON_lim,'ytick',LAT_lim,'fontsize',15,...
+        'xticklabels',LON_lim,'yticklabels',LAT_lim);
     hold on;
     mqr_ref = m_quiver(120+48.5/60,21+56.5/60,1000,0,0);
     mqr_ref.Color = 'w';
@@ -29,5 +34,9 @@ function ship_track_current_function(AnLLonDeg,AnLLatDeg,u,v,trajectory_ind,LON_
     set(mqr_ref,'UData',scale*hU_ref,'VData',scale*hV_ref)
     hold on;
     m_text(120+48.5/60,21+57/60,'1 m/s','Color','w','FontWeight','bold');
-
+    hold on;
+    m_text(120+44/60,21+59.5/60,['Start : ' char(time(trajectory_ind(1)))],...
+        'Color','w','FontWeight','bold');
+    m_text(120+44/60,21+58.5/60,['End : ' char(time(trajectory_ind(end)))],...
+        'Color','w','FontWeight','bold');
 end
